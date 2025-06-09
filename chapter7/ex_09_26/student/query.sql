539ade7ef84331dd80b7b930c66430df071799eb
DavidTrimble <david.trimble196@topper.wku.edu>
2025-06-09 22:23:25 +0000

    Testing Mon Jun 09 2025 22:23:25 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,8 +1,7 @@
 SELECT
-MIN(CUS_BALANCE) as "Minimum Balance",
-MAX(CUS_BALANCE) as "Maximum Balance",
-ROUND(AVG(CUS_BALANCE), 2) as "Average Balance"
+MIN(CUS_BALANCE) as 'Minimum Balance',
+MAX(CUS_BALANCE) as 'Maximum Balance',
+ROUND(AVG(CUS_BALANCE), 2) as 'Average Balance',
+SUM(CUS_BALANCE) as 'Total Balance'
 FROM
-CUSTOMER C
-JOIN
-INVOICE I ON C.CUS_CODE = I.CUS_CODE;
+CUSTOMER;
